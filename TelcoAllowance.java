6bdd148492ec4f7f1887032e86f44be168ddc33a
Angelica Toquero <152822754+AngelicaToquero@users.noreply.github.com>
2025-03-05 15:21:59 +0800

    Update TelcoAllowance.java

--- TelcoAllowance.java
+++ TelcoAllowance.java
@@ -3,4 +3,9 @@ public class TelcoAllowance implements UsagePromo {
     public String showAllowance(String telcoName, double price) {
         return telcoName + " offers a data plan for ₱" + price + ".";
     }
+
+
+    public String showAllowance(String telcoName, int dataAllowance, double price) {
+        return telcoName + " offers " + dataAllowance + "GB data plan for ₱" + price + ".";
+    }
 }
